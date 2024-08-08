c932d4b48abb46cd4690644531fcff44db0d0e65
YBM <sashapohh@gmail.com>
2024-08-08 17:56:05 +0200

    fix

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
-          <span className="text-2xl font-bold">Animal Catalog</span>
+          <span className="text-2xl font-bold cursor-pointer text-white">
+            Animal Catalog
+          </span>
         </Link>
       </div>
     </header>
